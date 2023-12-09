17cfdc44b4bc29707f2d3b7b3b4c5dc7109bb3a5
Alx_27 <didieralejandromejia83@gmail.com>
2023-12-08 20:30:25 -0500

    endpoint put users

--- routers/users.py
+++ routers/users.py
@@ -16,12 +16,19 @@ async def get_users():
 
 @router.get("/get-user", response_model=User_output, status_code=status.HTTP_200_OK)
 async def get_user(id: str):
-    return search_user("_id", ObjectId(id))
+    try:
+        user = search_user("_id", ObjectId(id))
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid request")
+    if type(user) == User_output:
+        return user
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="The user does not exist")
 
 @router.post("/", response_model=User_output, status_code=status.HTTP_201_CREATED)
 async def create_user(user: User):
     if type(search_user("email", user.email)) == User_output:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
+        raise HTTPException(status_code=status.HTTP_226_IM_USED, detail="The user already exists")
     
     new_user = dict(user)
     del new_user["id"]
@@ -33,9 +40,27 @@ async def create_user(user: User):
     
     return user_find
     
+@router.put("/", response_model=User_output, status_code=status.HTTP_200_OK)  
+async def update_user(user: User):
+    found_user = search_user("email", user.email)
+    if found_user != None:
+        if user.email == found_user.email and user.id != found_user.id:
+            raise HTTPException(status_code=status.HTTP_226_IM_USED, detail="That email is already in use by another user")
+    
+    new_user = dict(user)
+    del new_user["id"]
+    new_user["email"] = str(new_user["email"]).lower()
+    
+    try:
+        client_db.users.find_one_and_replace({"_id": ObjectId(user.id)}, new_user)
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid request")
+    
+    return search_user("_id", ObjectId(user.id))
+    
 def search_user(field: str, key):
     user_find = client_db.users.find_one({field: key})
     
     if user_find != None:
         return User_output(**user_schema(user_find))
-    return False
+    return None
